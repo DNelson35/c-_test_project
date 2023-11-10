99cfda6f280aef370a7c9f0b19b068beb371a127
Damien Nelson <dhnisaman@gmail.com>
2023-11-09 19:43:24 -0500

    testing method calling in c#

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+// Console.WriteLine("Hello C#!");
+Random dice = new Random();
+
+int roll1 = dice.Next(1,7);
+
+int roll2 = dice.Next(101);
+
+int roll3 = dice.Next(30, 40);
+
+Console.WriteLine($"first roll: {roll1}, second roll: {roll2}, third roll: {roll3}");

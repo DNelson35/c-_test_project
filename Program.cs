a1201175f33478103eaf3f40a30707a3ad7a65e4
Damien Nelson <dhnisaman@gmail.com>
2023-11-09 19:59:11 -0500

    using methods to find largest value

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello C#!");
-Random dice = new Random();
+// method calls and using random class
+// Random dice = new Random();
 
-int roll1 = dice.Next(1,7);
+// int roll1 = dice.Next(1,7);
 
-int roll2 = dice.Next(101);
+// int roll2 = dice.Next(101);
 
-int roll3 = dice.Next(30, 40);
+// int roll3 = dice.Next(30, 40);
 
-Console.WriteLine($"first roll: {roll1}, second roll: {roll2}, third roll: {roll3}");
+// Console.WriteLine($"first roll: {roll1}, second roll: {roll2}, third roll: {roll3}");
+
+// using math class to find the largest value
+// int smallvalue = 100;
+// int bigvalue = 100000;
+
+// int largestvalue;
+
+// largestvalue = Math.Max(smallvalue, bigvalue);
+
+// Console.WriteLine($"largest value: {largestvalue}");

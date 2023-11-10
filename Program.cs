64a5ca41e7e97e0c84b4c2580f74fc5aa84d6b5f
Damien Nelson <dhnisaman@gmail.com>
2023-11-09 21:13:10 -0500

    add code put a value at array location using loop

--- Program.cs
+++ Program.cs
@@ -90,6 +90,23 @@
 
 // Console.WriteLine($"the sum is {sum} total bins is {bin}");
 
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     int prefixValue = random.Next(65, 70);
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     string suffix = random.Next(1, 1000).ToString("000");
+
+//     orderIDs[i] = prefix + suffix;
+// }
+
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
 
 
 
